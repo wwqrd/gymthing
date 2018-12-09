b9381295373937ab11f9898532914bf63d398c52
Steve Mckellar <steve@wwqrd.uk>
2018-12-09 21:29:11 +0000

    Fix for relative path

---
+++ build/precache-manifest.6a3bd8adeba3a7359ecf52e6e902f830.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b5616f1a1c88992634",
+    "url": "/gymthing/static/js/runtime~main.e4b5616f.js"
+  },
+  {
+    "revision": "c13c76962565ca1c3a44",
+    "url": "/gymthing/static/js/main.c13c7696.chunk.js"
+  },
+  {
+    "revision": "784d010e315bf25234c6",
+    "url": "/gymthing/static/js/1.784d010e.chunk.js"
+  },
+  {
+    "revision": "c13c76962565ca1c3a44",
+    "url": "/gymthing/static/css/main.ef4f269d.chunk.css"
+  },
+  {
+    "revision": "ecb234eb983ce50331ce3d11d19c2392",
+    "url": "/gymthing/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.2ce8146654dbb9054bff1909b6cc2b1e.js"
+  "/gymthing/precache-manifest.6a3bd8adeba3a7359ecf52e6e902f830.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/gymthing/index.html", {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
