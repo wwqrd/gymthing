1911ffb4ecc078a97e5f4c753ba1a7a33ecf28e1
Steve Mckellar <steve@wwqrd.uk>
2018-12-09 21:24:34 +0000

    MVP Build

---
+++ precache-manifest.2ce8146654dbb9054bff1909b6cc2b1e.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "1415bd8d1bba2ab1c497",
+    "url": "/static/js/main.1415bd8d.chunk.js"
+  },
+  {
+    "revision": "784d010e315bf25234c6",
+    "url": "/static/js/1.784d010e.chunk.js"
+  },
+  {
+    "revision": "1415bd8d1bba2ab1c497",
+    "url": "/static/css/main.ef4f269d.chunk.css"
+  },
+  {
+    "revision": "50c1edf3b2034992c4d2b9a216bb1702",
+    "url": "/index.html"
+  }
+];
