89ba617867b2c0af58c00240ddffdf7a110be9aa
Steve Mckellar <steve@wwqrd.uk>
2018-12-13 10:24:22 +0000

    Build

---
+++ build/precache-manifest.2f8d7a0aaa96463c33f45f435788013d.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "e4b5616f1a1c88992634",
+    "url": "/gymthing/static/js/runtime~main.e4b5616f.js"
+  },
+  {
+    "revision": "39534c3e1e985c0f873f",
+    "url": "/gymthing/static/js/main.39534c3e.chunk.js"
+  },
+  {
+    "revision": "e30cafbacc03f43f4f7b",
+    "url": "/gymthing/static/js/1.e30cafba.chunk.js"
+  },
+  {
+    "revision": "39534c3e1e985c0f873f",
+    "url": "/gymthing/static/css/main.259fbd4a.chunk.css"
+  },
+  {
+    "revision": "578c316d976136a5a794e9803b76d2ed",
+    "url": "/gymthing/index.html"
+  }
+];
--- build/service-worker.js
+++ build/service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/gymthing/precache-manifest.73aece1e2fb2485b7b28246506db87a3.js"
+  "/gymthing/precache-manifest.2f8d7a0aaa96463c33f45f435788013d.js"
 );
 
 workbox.clientsClaim();
